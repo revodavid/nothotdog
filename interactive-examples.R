ebec1d6834e140751b54f21fb57d63fbd81c2970
David Smith <davidsmi@microsoft.com>
2018-09-06 13:07:03 -0500

    Example uses of hotdog_predict

---
+++ interactive-examples.R
@@ -0,0 +1,8 @@
+hotdog_predict("http://www.wienerschnitzel.com/wp-content/uploads/2014/10/hotdog_mustard-main.jpg")
+hotdog_predict("https://www.dairyqueen.com/Global/Food/Hot-Dogs_8-to-1_470x500.jpg?width=&height=810")
+
+hotdog_predict("http://www.hot-dog.org/sites/default/files/pictures/hot-dogs-on-the-grill-sm.jpg")
+
+hotdog_predict("https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Burrito_with_rice.jpg/1200px-Burrito_with_rice.jpg")
+
+hotdog_predict("https://www.recipetineats.com/wp-content/uploads/2017/09/Spring-Rolls-6.jpg")
